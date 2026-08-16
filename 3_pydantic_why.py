5defe290e8e4a9fc09f748dc27b4ee2a9f67b25b
SatyamKumar457 <satyamkumar1092004@gmail.com>
2026-08-16 14:29:58 +0530

    Any Url validator.

--- 3_pydantic_why.py
+++ 3_pydantic_why.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel,EmailStr
+from pydantic import BaseModel,EmailStr,AnyUrl
 from typing import List,Dict,Optional
 
 class Patient(BaseModel):
 
     name:str
     email:EmailStr
+    linkedin_url:AnyUrl
     age:int
     weight:float
     married:bool = False
@@ -19,10 +20,11 @@ def insert_patient_data(patient:Patient):
     print(patient.married)
     print(patient.allergies)
     print(patient.contact_details)
+    print(patient.linkedin_url)
     print("Inserted")
 
 
-patient_info = {'name':'Satyam','email':'satyam@gmail.com','age':'20','weight':'75','married':'False','allergies':['None'],'contact_details':{'phone':'309248','email':'cjsbfjb'}}
+patient_info = {'name':'Satyam','email':'satyam@gmail.com','age':'20','weight':'75','married':'False','allergies':['None'],'contact_details':{'phone':'309248','email':'cjsbfjb'},'linkedin_url':'http://satyam.com'}
 
 patient1 = Patient(**patient_info)
 
