ab8bf8c1ba09d6b8734093c5f4a3802d0d9ac8e2
SatyamKumar457 <satyamkumar1092004@gmail.com>
2026-08-16 14:43:47 +0530

    Using Field for custom data validation, set metadata, set default and restrict type conversion.

--- 3_pydantic_why.py
+++ 3_pydantic_why.py
@@ -1,15 +1,15 @@
-from pydantic import BaseModel,EmailStr,AnyUrl
-from typing import List,Dict,Optional
+from pydantic import BaseModel,EmailStr,AnyUrl, Field
+from typing import List,Dict,Optional,Annotated
 
 class Patient(BaseModel):
 
-    name:str
+    name: Annotated[str, Field(max_length=50, title="Name of the Patient", description="Give the name of the patient in less than 50 chars", examples=['Satyam','Amit'])]
     email:EmailStr
     linkedin_url:AnyUrl
-    age:int
-    weight:float
-    married:bool = False
-    allergies:Optional[List[str]]=None
+    age:int = Field(gt=0,lt=120)
+    weight:Annotated[float , Field(gt=0,strict=True)]
+    married: Annotated[bool, Field(default=None, description="Is the patient married or not.")]
+    allergies:Annotated[Optional[List[str]],Field(default=None,max_length=5)]
     contact_details:Dict[str,str]
 
 def insert_patient_data(patient:Patient):
@@ -24,7 +24,7 @@ def insert_patient_data(patient:Patient):
     print("Inserted")
 
 
-patient_info = {'name':'Satyam','email':'satyam@gmail.com','age':'20','weight':'75','married':'False','allergies':['None'],'contact_details':{'phone':'309248','email':'cjsbfjb'},'linkedin_url':'http://satyam.com'}
+patient_info = {'name':'Satyam','email':'satyam@gmail.com','age':'20','weight':75,'married':'False','allergies':['None'],'contact_details':{'phone':'309248','email':'cjsbfjb'},'linkedin_url':'http://satyam.com'}
 
 patient1 = Patient(**patient_info)
 
